233145b17217e3ea30a7ea377114946b6efa2cd0
danieldenton <danielmdenton@gmail.com>
2022-08-06 17:41:18 -0700

    counter

--- dayplanner/daily/views.py
+++ dayplanner/daily/views.py
@@ -1,7 +1,17 @@
+from lzma import FORMAT_ALONE
 from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Fool',
+        'age': '44',
+        'nationality': 'Irish'
+    } 
+    return render(request, 'index.html', context)
+
+def counter(request):
+    return render(request, 'counter.html')
+
