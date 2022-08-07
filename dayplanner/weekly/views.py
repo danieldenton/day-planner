368c47e34a3bb1e907067cb8216abb0f811f3f08
danieldenton <danielmdenton@gmail.com>
2022-08-06 17:06:21 -0700

    httpresponse

---
+++ dayplanner/weekly/views.py
@@ -0,0 +1,7 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Yo</h1>')
