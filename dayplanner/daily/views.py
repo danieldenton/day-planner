c99b63bd8067851ca4e5ea8a6dc6fe0d9db3637d
danieldenton <danielmdenton@gmail.com>
2022-08-07 13:48:29 -0700

    signup form

--- dayplanner/daily/views.py
+++ dayplanner/daily/views.py
@@ -1,12 +1,15 @@
-from lzma import FORMAT_ALONE
-from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TaskList, Task
 
 # Create your views here.
 def home(request):  
     return render(request, 'index.html')
 
-def login(request):    
-    return render(request, 'login.html')
+def signup(request):    
+    return render(request, 'signup.html')
+
+def daily(request, id):
+    tl = TaskList.objects.get(id=id)
+    return HttpResponse("<h1>%s" % tl.name)
 
